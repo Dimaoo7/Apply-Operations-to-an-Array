1dbdf97ee558823fe9bda0d8b81a4b8be1360e60
dimak <dimakrjukov@gmail.com>
2025-03-07 15:18:49 +0100

    init commit

---
+++ src/main/java/system/order/Solution.java
@@ -0,0 +1,19 @@
+package system.order;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args){
+
+        int[] nums = {1, 2, 2, 1, 1, 0};
+
+        System.out.println(Arrays.toString(applyOperations(nums)));
+
+    }
+
+    public static int[] applyOperations(int[] nums) {
+        int[] result = new int[nums.length];
+
+        return result;
+    }
+}
